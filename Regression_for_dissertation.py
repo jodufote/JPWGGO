6819f216417901d5d2d9d326bd4219d835c45fa6
jodufote <67468768+jodufote@users.noreply.github.com>
2020-06-26 19:17:25 +0100

    Package build up 1

---
+++ Regression_for_dissertation.py
@@ -0,0 +1,21 @@
+# The Environment:
+''' # we need `pandas` for data entry, web scraping, and other forms of data analysis. 
+# numpy - arrays - vectors and numerical anaysis
+# matplotlib for plots scatters, 
+# %matplot... - speciifcally for jupyter note 
+
+
+'''
+
+import pandas as pd 
+import numpy as np
+import matplotlib as mpl 
+%matplot inline 
+import scipy import sp
+import sklearn 
+
+
+
+
+
+import matplotlib.pyplot as plt
